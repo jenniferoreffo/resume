3c0dd23d293ceb5dd4fc8a55d489c29b813b35e6
Jennifer Oreffo <57407703+jenniferoreffo@users.noreply.github.com>
2022-12-01 18:20:16 +0100

    Update jquery.prettyPhoto.js

--- js/jquery.prettyPhoto.js
+++ js/jquery.prettyPhoto.js
@@ -1,7 +1,6 @@
 /* ------------------------------------------------------------------------
 	Class: prettyPhoto
 	Use: Lightbox clone for jQuery
-	Author: Stephane Caron (http://www.no-margin-for-errors.com)
 	Version: 3.1.4
 ------------------------------------------------------------------------- */
 
